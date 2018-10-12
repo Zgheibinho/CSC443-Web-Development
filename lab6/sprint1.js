847a40135d1917fd28e79957afc102c7ee076370
moussa <moussa.zoghaib@lau.edu>
2018-10-12 11:59:01 +0300

    lab6

---
+++ lab6/sprint1.js
@@ -0,0 +1,12 @@
+class Card {
+    constructor(id,src) {
+      this.id=id;
+      this.src=src;
+      this.front="card_hs.png";
+      this.imghtml= document.createElement("img");
+    }
+
+    changesrc(){
+      
+    }
+}
